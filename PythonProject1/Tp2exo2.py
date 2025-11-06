50b18d463f8d3bc27e1bf2d0bdfa1a66082e2f9b
Alessandro-Bauer-Sicari <alessandro68260@gmail.com>
2025-11-06 16:46:37 +0100

    Delete PythonProject1 directory

--- PythonProject1/Tp2exo2.py
+++
@@ -1,4 +0,0 @@
-
-age = int(input("Donnez votre age : "))
-annee = 2025 - age
-print("Votre annee de naissance est :", annee)
--- PythonProject1/tp2exo7.py
+++
@@ -1,8 +0,0 @@
-import random
-
-tirage = random.randint(0, 2)
-
-if tirage < 2:
-    print("Pile !")
-else:
-    print("Face !")
