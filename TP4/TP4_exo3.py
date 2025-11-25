f72e2289da244ceb222cea490af0f8e1b51f3178
Alessandro-Bauer-Sicari <alessandro68260@gmail.com>
2025-11-25 16:26:36 +0100

    maj

---
+++ TP4/TP4_exo3.py
@@ -0,0 +1,19 @@
+nMax=3
+v1=[]
+v2=[]
+n = int(input(f"Quelle est la taille de vos vecteurs [entre 1 et {nMax}] ? "))
+
+while n < 1 or n > nMax:
+    n = int(input(f"Valeur incorrecte ! Entrez une taille entre 1 et {nMax} : "))
+print("Saisie du premier vecteur :")
+for i in range(n):
+    valeur = int(input(f"v1[{i}] = "))
+    v1.append(valeur)
+print("Saisie du second vecteur :")
+for i in range(n):
+    valeur = int(input(f"v2[{i}] = "))
+    v2.append(valeur)
+produit_scalaire = 0
+for i in range(n):
+    produit_scalaire += v1[i] * v2[i]
+print(f"Le produit scalaire de v1 par v2 vaut {float(produit_scalaire)}")
---
+++ TP4/tp3EXO4.py
@@ -0,0 +1,40 @@
+L = [2, 7, 5, 6, 7, 1, 6, 2, 1, 7]
+
+max_occurrences = 0
+element_frequent = L[0]
+
+for i in range(len(L)):
+    compteur = 0
+    for j in range(len(L)):
+        if L[i] == L[j]:
+            compteur += 1
+    if compteur > max_occurrences:
+        max_occurrences = compteur
+        element_frequent = L[i]
+
+print(f"Le nombre le plus frequent dans la liste est le : {element_frequent} ({max_occurrences} x)")
+
+L = [2, 7, 5, 6, 7, 1, 6, 2, 1, 7, 6]
+
+max_occurrences = 0
+element_frequent = L[0]
+
+for x in L:
+    compteur = L.count(x)
+    if compteur > max_occurrences:
+        max_occurrences = compteur
+        element_frequent = x
+
+print(f"Le nombre le plus frequent dans la liste est le : {element_frequent} ({max_occurrences} x)")
+L = [2, 7, 5, 6, 7, 1, 6, 2, 1, 7, 6]
+
+max_occurrences = 0
+element_frequent = L[0]
+
+for x in L:
+    compteur = L.count(x)
+    if compteur > max_occurrences:
+        max_occurrences = compteur
+        element_frequent = x
+
+print(f"Le nombre le plus frequent dans la liste est le : {element_frequent} ({max_occurrences} x)")
